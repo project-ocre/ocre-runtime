7c9cb0de222de123729bb619e5555819c1fa76a7
Tyler Goulding <tfgoulding@gmail.com>
2024-08-21 10:23:17 -0700

    bug: fix potential buff overflow
    -) subs out strcpy for strlcat.
      -) fixes potential buf overflow on native target where info.version is 70 char
    -) bumps up buffer size to fit info.version

--- src/ocre/api/ocre_api.h
+++ src/ocre/api/ocre_api.h
@@ -10,17 +10,19 @@
 #ifndef OCRE_API_H
 #define OCRE_API_H
 
+#define OCRE_API_POSIX_BUF_SIZE 75
+
 #ifndef OCRE_SYSTEM_NAME
 #define OCRE_SYSTEM_NAME "Project Ocre"
 #endif
 
 struct _ocre_posix_utsname {
-    char sysname[65];
-    char nodename[65];
-    char release[65];
-    char version[65];
-    char machine[65];
-    char domainname[65];
+    char sysname[OCRE_API_POSIX_BUF_SIZE];
+    char nodename[OCRE_API_POSIX_BUF_SIZE];
+    char release[OCRE_API_POSIX_BUF_SIZE];
+    char version[OCRE_API_POSIX_BUF_SIZE];
+    char machine[OCRE_API_POSIX_BUF_SIZE];
+    char domainname[OCRE_API_POSIX_BUF_SIZE];
 };
 
 int _ocre_posix_uname(wasm_exec_env_t exec_env, struct _ocre_posix_utsname *name);
