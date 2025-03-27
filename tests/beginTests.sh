442d5f99d0573ba8a2dc45dfc9948a62edaa00f6
Evan Parker <eparker@iol.unh.edu>
2025-03-27 16:22:24 -0400

    Start working on workflow and bash scripting

--- tests/beginTests.sh
+++ tests/beginTests.sh
@@ -1,10 +1,12 @@
-# 1. Find all directories in ./groups
+#!/bin/bash
 
-# 2. For each group:
-# 2.1. Read config.json
-# 2.2. Write to global log saying "Begining <JSON.name> (<JSON.description>)"
-# 2.3. For every entry in <JSON.setup>, execute the script there
-# 2.4. For every test, execute the test. if there is a fail, break out of the suite or test
-# 2.5. Run cleanp scripts
+TG=$1
+LOGFILE="/tmp/$TG.log"
 
-# 3. Write logs and results back to GitHub workflow
+NAME=$(cat groups/$TG/config.json | jq .name)
+DESCRIPTION=$(cat groups/$TG/config.json | jq .description)
+
+rm $LOGFILE
+touch $LOGFILE
+echo "Beginning test group $NAME" >> $LOGFILE
+echo $DESCRIPTION >> $LOGFILE
