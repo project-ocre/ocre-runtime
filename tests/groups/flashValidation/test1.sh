9694924780c528157accf24468b812e79160986b
Patrick Robb <probb@iol.unh.edu>
2025-04-14 21:25:19 -0400

    ocre-tests: add flash validation test cases
    Add flash validation test cases, and remove placeholder testcases.
    
    Signed-off-by: Patrick Robb <probb@iol.unh.edu>

--- tests/groups/flashValidation/test1.sh
+++
@@ -1,8 +0,0 @@
-echo "Will this test pass?"
-if [[ $(($RANDOM % 2)) == 0 ]]; then
-    echo "Test passed!"
-    exit 0
-fi
-
-echo "Test failed"
-exit 1
--- tests/groups/flashValidation/test2.sh
+++
@@ -1,4 +0,0 @@
-echo "Doing some heavy computation (1 + 1)..."
-sleep 5
-echo "Test failed, 1 + 1 = 3 is FALSE"
-exit 1
