9719d847a8f0b8799563d4927d5b13124726336f
Marco Casaroli <marco.casaroli@gmail.com>
2025-11-17 15:37:50 +0100

    feat(zephyr): use shared multi heap for user heap
    These functions allocate memory on external PSRAM on boards that
    have CONFIG_SHARED_MULTI_HEAP enabled.
    
    Otherwise, they work just like core_malloc and core_free.
    
    Signed-off-by: Marco Casaroli <marco.casaroli@gmail.com>

--- src/shared/platform/zephyr/core_memory.c
+++ src/shared/platform/zephyr/core_memory.c
@@ -8,6 +8,7 @@
 #include "ocre_core_external.h"
 
 #include <zephyr/kernel.h>
+#include <zephyr/multi_heap/shared_multi_heap.h>
 
 void *core_malloc(size_t size) {
     return k_malloc(size);
@@ -16,3 +17,22 @@ void *core_malloc(size_t size) {
 void core_free(void *ptr) {
     k_free(ptr);
 }
+
+#ifdef CONFIG_SHARED_MULTI_HEAP
+void *user_malloc(size_t size) {
+    return shared_multi_heap_aligned_alloc(SMH_REG_ATTR_EXTERNAL, 32, size);
+}
+
+void user_free(void *ptr) {
+    shared_multi_heap_free(ptr);
+}
+#else
+#warning CONFIG_SHARED_MULTI_HEAP is not defined. Using internal RAM
+void *user_malloc(size_t size) {
+    return k_malloc(size);
+}
+
+void user_free(void *ptr) {
+    k_free(ptr);
+}
+#endif
