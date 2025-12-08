8c8267efb18aba02b8a4236e223a52ec0a6549fc
Marco Casaroli <marco.casaroli@gmail.com>
2025-12-08 22:08:52 +0100

    feat(samples): demo
    Some random container management.
    
    Signed-off-by: Marco Casaroli <marco.casaroli@gmail.com>

--- src/samples/demo/main.c
+++ src/samples/demo/main.c
@@ -10,6 +10,7 @@ const struct ocre_container_args args = {
 int main(int argc, char *argv[])
 {
 	int rc;
+	int status;
 
 	rc = ocre_initialize();
 	if (rc) {
@@ -64,7 +65,6 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	int status;
 	rc = ocre_container_wait(blinky, &status);
 	if (rc) {
 		fprintf(stderr, "Failed to wait for container\n");
@@ -73,75 +73,87 @@ int main(int argc, char *argv[])
 
 	fprintf(stderr, "Container exited with status %d\n", status);
 
-	// rc = ocre_context_remove_container(ocre, blinky);
-	// if (rc) {
-	// 	fprintf(stderr, "Failed to remove container\n");
-	// 	return 1;
-	// }
+	rc = ocre_context_remove_container(ocre, blinky);
+	if (rc) {
+		fprintf(stderr, "Failed to remove container\n");
+		return 1;
+	}
 
-	// struct ocre_container *subscriber =
-	// 	ocre_context_create_container(ocre, "subscriber.wasm", "wamr", NULL, true, &args);
+	struct ocre_container *subscriber =
+		ocre_context_create_container(ocre, "subscriber.wasm", "wamr", NULL, true, &args);
 
-	// if (!subscriber) {
-	// 	fprintf(stderr, "Failed to create container\n");
-	// 	return 1;
-	// }
+	if (!subscriber) {
+		fprintf(stderr, "Failed to create container\n");
+		return 1;
+	}
 
-	// struct ocre_container *publisher =
-	// 	ocre_context_create_container(ocre, "publisher.wasm", "wamr", NULL, true, &args);
+	struct ocre_container *publisher =
+		ocre_context_create_container(ocre, "publisher.wasm", "wamr", NULL, true, &args);
 
-	// if (!publisher) {
-	// 	fprintf(stderr, "Failed to create container\n");
-	// 	return 1;
-	// }
+	if (!publisher) {
+		fprintf(stderr, "Failed to create container\n");
+		return 1;
+	}
 
-	// rc = ocre_container_start(subscriber);
-	// if (rc) {
-	// 	fprintf(stderr, "Failed to start container\n");
-	// 	return 1;
-	// }
+	rc = ocre_container_start(subscriber);
+	if (rc) {
+		fprintf(stderr, "Failed to start container\n");
+		return 1;
+	}
 
-	// sleep(5);
+	sleep(1);
 
-	// rc = ocre_container_start(publisher);
-	// if (rc) {
-	// 	fprintf(stderr, "Failed to start container\n");
-	// 	return 1;
-	// }
+	rc = ocre_container_start(publisher);
+	if (rc) {
+		fprintf(stderr, "Failed to start container\n");
+		return 1;
+	}
 
-	// sleep(10);
+	sleep(5);
 
-	// rc = ocre_container_kill(subscriber);
-	// if (rc) {
-	// 	fprintf(stderr, "Failed to kill container\n");
-	// 	return 1;
-	// }
+	rc = ocre_container_kill(subscriber);
+	if (rc) {
+		fprintf(stderr, "Failed to kill container\n");
+		return 1;
+	}
 
-	// rc = ocre_container_kill(publisher);
-	// if (rc) {
-	// 	fprintf(stderr, "Failed to kill container\n");
-	// 	return 1;
-	// }
+	rc = ocre_container_kill(publisher);
+	if (rc) {
+		fprintf(stderr, "Failed to kill container\n");
+		return 1;
+	}
 
-	// rc = ocre_container_wait(subscriber, &status);
-	// if (rc) {
-	// 	fprintf(stderr, "Failed to wait for container\n");
-	// 	return 1;
-	// }
+	rc = ocre_container_wait(subscriber, &status);
+	if (rc) {
+		fprintf(stderr, "Failed to wait for container\n");
+		return 1;
+	}
 
-	// fprintf(stderr, "Container exited with status %d\n", status);
+	fprintf(stderr, "Subscriber exited with status %d\n", status);
 
-	// rc = ocre_container_wait(publisher, &status);
-	// if (rc) {
-	// 	fprintf(stderr, "Failed to wait for container\n");
-	// 	return 1;
-	// }
+	rc = ocre_container_wait(publisher, &status);
+	if (rc) {
+		fprintf(stderr, "Failed to wait for container\n");
+		return 1;
+	}
 
-	// fprintf(stderr, "Container exited with status %d\n", status);
+	fprintf(stderr, "Publisher exited with status %d\n", status);
 
-	// ocre_destroy_context(ocre);
+	rc = ocre_context_remove_container(ocre, subscriber);
+	if (rc) {
+		fprintf(stderr, "Failed to remove subscriber\n");
+		return 1;
+	}
 
-	// ocre_deinitialize();
+	rc = ocre_context_remove_container(ocre, publisher);
+	if (rc) {
+		fprintf(stderr, "Failed to remove publisher\n");
+		return 1;
+	}
+
+	ocre_destroy_context(ocre);
+
+	ocre_deinitialize();
 
 	return 0;
 }
