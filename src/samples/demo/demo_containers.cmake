9015d7017b15565cd4ec8c55a2562cbf0ca02816
Marco Casaroli <marco.casaroli@gmail.com>
2025-12-10 14:11:36 +0100

    feat(samples/demo): add sample containers to build
    Signed-off-by: Marco Casaroli <marco.casaroli@gmail.com>

---
+++ src/samples/demo/demo_containers.cmake
@@ -0,0 +1,10 @@
+list(APPEND OCRE_SDK_PRELOADED_IMAGES
+    "hello-world.wasm"
+    "blinky.wasm"
+    "subscriber.wasm"
+    "publisher.wasm"
+    "filesystem.wasm"
+    "filesystem-full.wasm"
+    "webserver.wasm"
+    "webserver-complex.wasm"
+)
