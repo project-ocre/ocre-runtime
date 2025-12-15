6ee63734e281b54a697109bd9685cef4248d9e69
Marco Casaroli <marco.casaroli@gmail.com>
2025-12-15 14:55:40 +0100

    feat(shell): ocre image pull
    Will pull an image from http server.
    
    Signed-off-by: Marco Casaroli <marco.casaroli@gmail.com>

--- src/shell/image/pull.c
+++ src/shell/image/pull.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
 #include <ocre/ocre.h>
 
 #include "../command.h"
+#include "sha256_file.h"
+
+extern int ocre_download_file(const char *url, const char *filepath);
 
 static int usage(const char *argv0)
 {
@@ -14,16 +19,76 @@ static int usage(const char *argv0)
 
 int cmd_image_pull(struct ocre_context *ctx, char *argv0, int argc, char **argv)
 {
-	if (argc < 1) {
+	char *local_name = NULL;
+	int ret = -1;
+
+	if (argc < 2) {
 		fprintf(stderr, "'%s image pull' requires at least one argument\n\n", argv0);
 		return usage(argv0);
 	}
 
-	if (argc == 1) {
-		printf("Pulling image '%s'...\n", argv[1]);
+	if (argc == 2) {
+		local_name = strrchr(argv[1], '/');
+		if (local_name == NULL) {
+			fprintf(stderr, "'Cannot determine image name from URL '%s'\n", argv[1]);
+			return -1;
+		} else {
+			local_name++;
+		}
+		if (*local_name == '\0') {
+			fprintf(stderr, "'Cannot determine image name from URL '%s'\n", argv[1]);
+			return -1;
+		}
+
 	} else {
-		printf("Pulling image '%s' and saving locally as '%s'...\n", argv[1], argv[2]);
+		local_name = argv[2];
+
+		if (strchr(local_name, '/')) {
+			fprintf(stderr, "Local file '%s' cannot contain '/'\n", argv[2]);
+			return -1;
+		}
 	}
 
-	return 0;
+	char *working_directory = ocre_context_get_working_directory(ctx);
+
+	char *image_path = malloc(strlen(working_directory) + strlen("/images/") + strlen(local_name) + 1);
+	if (!image_path) {
+		fprintf(stderr, "Failed to allocate memory for image directory path\n");
+		return -1;
+	}
+
+	strcpy(image_path, working_directory);
+	strcat(image_path, "/images/");
+	strcat(image_path, local_name);
+
+	/* Check if image already exists */
+
+	struct stat st;
+
+	ret = stat(image_path, &st);
+	if (!ret) {
+		fprintf(stderr, "Image '%s' already exists\n", local_name);
+		goto finish;
+	}
+
+	fprintf(stderr, "Pulling '%s' from '%s'\n", local_name, argv[1]);
+
+	ret = ocre_download_file(argv[1], image_path);
+	if (ret) {
+		fprintf(stderr, "Failed to download image '%s'\n", argv[1]);
+		goto finish;
+	}
+
+	char hash[65] = {0}; /* hash[64] is null-byte */
+
+	sha256_file(image_path, hash);
+
+	fprintf(stderr, "Digest: %s\n", hash);
+
+	fprintf(stdout, "%s\n", local_name);
+
+finish:
+	free(image_path);
+
+	return ret;
 }
