4b48aa55c032e2a9d26987faf0ecf4c927227bf8
Marco Casaroli <marco.casaroli@gmail.com>
2025-12-17 11:02:22 +0100

    chore: remove spurious sample
    It was just a proof of concept.
    
    This will be added back properly later.
    
    Signed-off-by: Marco Casaroli <marco.casaroli@gmail.com>

--- src/samples/bash_runtime/posix/bash/include/ocre/runtime/bash/bash.h
+++
@@ -1,8 +0,0 @@
-#ifndef OCRE_RUNTIME_BASH_H
-#define OCRE_RUNTIME_BASH_H
-
-#include <ocre/runtime/vtable.h>
-
-extern const struct ocre_runtime_vtable bash_vtable;
-
-#endif /* OCRE_RUNTIME_BASH_H */
