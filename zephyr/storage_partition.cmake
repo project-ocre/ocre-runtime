69ccc68d0b0e6eb9466f213ae05579cda01d64d0
Marco Casaroli <marco.casaroli@gmail.com>
2026-02-03 19:40:00 +0100

    feat(zephyr): generate flash.bin in native_sim build
    Generate flash.bin like we do with merged.hex
    
    Signed-off-by: Marco Casaroli <marco.casaroli@gmail.com>

--- zephyr/storage_partition.cmake
+++ zephyr/storage_partition.cmake
@@ -15,22 +15,44 @@ add_custom_command(OUTPUT user_data.bin
     VERBATIM
 )
 
-add_custom_command(OUTPUT user_data.hex
-    COMMAND bin2hex.py
-        --offset ${CONFIG_OCRE_STORAGE_PARTITION_ADDR}
-        user_data.bin user_data.hex
-    DEPENDS user_data.bin
-    COMMENT "Generating user_data.hex"
-    VERBATIM
-)
-
-add_custom_target(user_data_partition ALL
-    DEPENDS user_data.hex
-    COMMENT "Generating user data partition"
-    VERBATIM
-)
-
 if (CONFIG_OCRE_MERGE_HEX)
+    add_custom_command(OUTPUT user_data.hex
+        COMMAND bin2hex.py
+            --offset ${CONFIG_OCRE_STORAGE_PARTITION_ADDR}
+            user_data.bin user_data.hex
+        DEPENDS user_data.bin
+        VERBATIM
+    )
+
+    add_custom_target(user_data_partition ALL
+        DEPENDS user_data.hex
+        COMMENT "Generating user data partition (user_data.hex)"
+        VERBATIM
+    )
+
     set_property(GLOBAL APPEND PROPERTY HEX_FILES_TO_MERGE "${CMAKE_CURRENT_BINARY_DIR}/user_data.hex")
     set_property(GLOBAL APPEND PROPERTY HEX_FILES_TO_MERGE "zephyr.hex")
+endif()
+
+if (CONFIG_BOARD_NATIVE_SIM)
+    # get the total flash size
+    math(EXPR total_flash_size "${CONFIG_OCRE_STORAGE_PARTITION_ADDR} + ${CONFIG_OCRE_STORAGE_PARTITION_SIZE}")
+
+    # convert to decimal if necessary
+    math(EXPR flash_offset "${CONFIG_OCRE_STORAGE_PARTITION_ADDR}")
+
+    # first we fill the total flash size with 0xff
+    # then we rewrite the storage_partition
+    add_custom_command(OUTPUT flash.bin
+        COMMAND dd if=/dev/zero bs=${total_flash_size} count=1 | tr '\\0' '\\377' | dd of=${PROJECT_BINARY_DIR}/../flash.bin
+        COMMAND dd if=user_data.bin of=${PROJECT_BINARY_DIR}/../flash.bin seek=1 bs=${flash_offset} conv=notrunc
+        DEPENDS user_data.bin
+        VERBATIM
+    )
+
+    add_custom_target(user_data_partition_native ALL
+        DEPENDS flash.bin
+        COMMENT "Generating user data partition (flash.bin)"
+        VERBATIM
+    )
 endif()
