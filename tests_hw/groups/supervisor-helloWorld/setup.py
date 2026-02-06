b371f3c56ce523c76075fd203a4b2f949dd1dc83
Matthew Gee <mgee@iol.unh.edu>
2026-02-06 17:18:42 -0500

    tests: Address initial pull request comments
    Code style, naming conventions, and formatting fixes.
    
    Signed-off-by: Matthew Gee <mgee@iol.unh.edu>

--- tests_hw/groups/supervisor-helloWorld/setup.py
+++ tests_hw/groups/supervisor-helloWorld/setup.py
@@ -19,9 +19,6 @@ def main():
     if "Failed to create container" in response:
         sys.exit(1)
     
-
-
-
-
+    
 if __name__ == "__main__":
     main()
--- tests_hw/groups/supervisor-helloWorld/testcase.py
+++ tests_hw/groups/supervisor-helloWorld/testcase.py
@@ -22,6 +22,5 @@ def main():
     sys.exit(1)
 
     
-
 if __name__ == "__main__":
     main()
