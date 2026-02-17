d99eefa08f4930c547473f7bfec5a9c739bb2c9f
SorinO <sorin.olari2@gmail.com>
2026-02-17 21:27:14 +0200

    CI(formatting-checks): use sh instead of bash to ensure compatibility (BASH_SOURCE only in bash)
    Signed-off-by: SorinO <sorin.olari2@gmail.com>

--- scripts/yml_linter.sh
+++ scripts/yml_linter.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # @copyright Copyright (c) contributors to Project Ocre,
 # which has been established as Project Ocre a Series of LF Projects, LLC
 #
@@ -9,7 +9,7 @@
 
 set -e
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 ROOT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
 
 echo "Checking YAML files."
