bf88000ab5c2ac37a3d421a3fb5c9c4e6605b66f
Matthew Gee <mgee@iol.unh.edu>
2026-02-20 13:34:47 -0500

    tests_hw: Clear serial buffer
    Clear serial buffer at the start and end of every serial connection
    during test cases to ensure runtime outputs from other github actions
    runs do not affect other runs.
    
    Also includes formatting updates in GHA workflow files and separates the
    demo and mini flashes to their own jobs outside of the container.
    
    Signed-off-by: Matthew Gee <mgee@iol.unh.edu>

--- tests_hw/groups/demo/testcase.py
+++ tests_hw/groups/demo/testcase.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import sys
+sys.path.append("../..")
+
 import pexpect
 import testlib
 
--- tests_hw/testlib.py
+++ tests_hw/testlib.py
@@ -8,12 +8,16 @@ from typing import Tuple
 def setup(device_filepath: str) -> Tuple[serial.Serial, pexpect.fdpexpect.fdspawn]:
     conn = serial.Serial(device_filepath, 115200, timeout=10)
     conn.reset_input_buffer()
+    conn.reset_output_buffer()
+    conn.flush()
     pex = pexpect.fdpexpect.fdspawn(conn.fileno())
     return (conn, pex)
 
 
 def full_exit(conn: serial.Serial, status: int):
+    conn.reset_input_buffer()
     conn.reset_output_buffer()
+    conn.flush()
     conn.close()
     sys.exit(status)
 
