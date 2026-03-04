f7c877c6b1256e7bf5f90364808af2d1fbba1467
SorinO <sorin.olari2@gmail.com>
2026-03-04 11:33:55 +0200

    scripts: modify comment and folders to exclude
    Signed-off-by: SorinO <sorin.olari2@gmail.com>

--- scripts/check_license_banner_conf_files.sh
+++ scripts/check_license_banner_conf_files.sh
@@ -4,7 +4,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-# Checks all C/C++ files for license banner
+# Checks license banner in all files that use '#' comment syntax
 
 set -e
 
@@ -45,13 +45,10 @@ ERROR_FOUND=0
 for file in $(find . -type f \( -name 'CMakeLists.txt' -o -name '*.yaml' \
     -o -name '*.yml' -o -name '*.awk' -o -name '*.conf' -o -name 'Kconfig*' \
     -o -name '.gitignore' -o -name '.gitmodules' -o -name '*.sh' -o -name '*.cmake' -o -name '*.py' \) \
-    ! -name 'utlist.h' \
     ! -path './tests/Unity/*' \
     ! -path './build/*' \
     ! -path './wasm-micro-runtime/*' \
     ! -path './ocre-sdk/*' \
-    ! -path './src/shell/sha256/*' \
-    ! -path './src/runtime/wamr-wasip1/ocre_api/utils/*' \
     | sort); do
 
     if ! check_license_banner "$file"; then
